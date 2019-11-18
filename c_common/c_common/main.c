823bd9b780f7574bf394161679eaed108ef718d7
Viacheslav <kykypyky@gmail.com>
2019-11-18 17:48:18 +0300

    Begining

---
+++ c_common/c_common/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  c_common
+//
+//  Created by Вячеслав Халиуллин on 18.11.2019.
+//  Copyright © 2019 Viacheslav Khaliullin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    
+    return 0;
+}
