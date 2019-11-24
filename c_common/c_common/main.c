c0a612f24d188c97b53be0bfcc49c416fd7b61db
Viacheslav <kykypyky@gmail.com>
2019-11-24 20:53:34 +0300

    F! Add main.c

--- c_common/c_common/main.c
+++ c_common/c_common/main.c
@@ -1,17 +1,94 @@
-//
-//  main.c
-//  c_common
-//
-//  Created by Вячеслав Халиуллин on 18.11.2019.
-//  Copyright © 2019 Viacheslav Khaliullin. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int args, const char* argv[]){
+    
+    int a = 10;
+    int b = 20;
+    int even = 0;
+    
+    while (a < b) {
+        if (a%2 == 0) even++;
+        a++;
+    }
+    printf("There are %d even numbers in sequence.\n", even);
+    
+    int input;
+    do {
+        printf("Enter a divider for 100 (Remember, that you can`t divide by zero)\n");
+        scanf("%d", &input);
+    } while (input == 0);
+    
+    printf("100 / %d = %d\n", input, 100/input);
+    printf("and reminder well be %d, by the way.\n", 100 % input);
     
     
     return 0;
 }
+    
+    
+    
+    
+/*    char answer;
+    printf("Do you want me to salute you? (y/n)");
+    scanf("%c", &answer);
+    if (answer == 'y' ) {
+        printf("Salute! How are you?\n");
+    } else if(answer == 'n'){
+        printf("I didn`t want to salute you anyway!\n");
+    } else {
+        printf("I can`t understand your input!\n");
+    }
+*/
+//    int a = 10;
+//    int b = 20;
+//
+//    a = (a>b) ? b : a;
+//    if (a > b) {
+//        a = b;
+//    } else {
+//        a = 0;
+//    }
+//    printf("\n%s", ((1)>0) ? "true\n" : "false\n");
+    
+    // > < >= <= ==
+    //&& || ! ^
+    
+//    if ((1) && 0) printf("false");
+//    if (((1) || 0)) printf("true");
+//    if ((!(1))) printf("false");
+//    if (1 ^ 1) printf("false");
+    
+    //      влорженные
+    //    if (1) {
+    //        if (1){
+    //           //...
+    //        }
+    //    }
+    
+//    int x = 7;
+//    if (x >= 0 && x <= 10){
+//        printf("\nX Fits!\n");
+//    }
+    
+
+
+
+
+//
+//#include <stdio.h>
+//
+//int main(int argc, const char * argv[]) {
+//    int variable = 70;
+//    printf("Переменная %d", variable);
+//
+//    variable += 100;
+//    printf("+100 = %d\n", variable);
+//    variable = variable / 4;
+//    printf("Целочисленное деление на 4: %d\n", variable);
+//    int reminder = variable % 4;
+//    printf("Остаток от деления равен: %d\n", reminder);
+//
+//
+//    return 0;
+//}
