c2d89ebaa4c996239775c23ff40d58d7aa151a58
Viacheslav Khaliullin <v.haliullin@pixonic.com>
2020-01-16 19:55:32 +0300

    F! 01 done

--- c_algorithms/01/01_wieght.c
+++ c_algorithms/01/01_wieght.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-double humanParam(double weight, double height)
-{
-    return weight / (height * height);
-}
-
-
 int main(int argc, const char *argv[])
 {
     double weight, height;
     
-    printf("Enter weight: ");
-    scanf("%lf", weight);
-    printf("Enter height: ");
-    scanf("%lf", height);
+    printf("Enter weight KG: ");
+    scanf("%lf", &weight);
+    printf("Enter height METERS: ");
+    scanf("%lf", &height);
+
+    printf("Mass Index: %lf", weight / pow(height, 2));
 
    
     
